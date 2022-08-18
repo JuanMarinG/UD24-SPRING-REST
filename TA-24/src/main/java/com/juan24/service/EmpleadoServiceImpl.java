2c4ccb003ed3686e900ee092b1d980e639586069
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-08-18 20:31:09 +0200

    Update EmpleadoServiceImpl.java

--- TA-24/src/main/java/com/juan24/service/EmpleadoServiceImpl.java
+++ TA-24/src/main/java/com/juan24/service/EmpleadoServiceImpl.java
@@ -1,5 +1,43 @@
 package com.juan24.service;
 
-public class EmpleadoServiceImpl {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.juan24.dao.IempleadoDAO;
+import com.juan24.dto.Empleado;
+
+@Service
+public class EmpleadoServiceImpl implements IempleadoService{
+
+	
+	@Autowired
+	IempleadoDAO iempleadoDAO;
+	
+	public List<Empleado> listarEmpleados(){
+		return iempleadoDAO.findAll();
+	}
+	
+	@Override
+	public Empleado guardarEmpleado(Empleado empleado) {
+		return iempleadoDAO.save(empleado);
+		
+	
+	}
+
+	@Override
+	public Empleado empleadosPorId(Long Id) {
+		return iempleadoDAO.findById(Id).get();
+	}
+
+	@Override
+	public Empleado actualizarEmpleado(Empleado empleado) {
+		return iempleadoDAO.save(empleado);
+	}
+
+	@Override
+	public void eliminarEmpleado(Long Id) {
+		iempleadoDAO.deleteById(Id);
+	}
 }
